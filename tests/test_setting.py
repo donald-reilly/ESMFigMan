c12e1c919e434aefdbb4a01574e02173d502717a
Donald Reilly Jr <donald.reilly.jr@outlook.com>
2025-07-23 17:16:06 -0700

    Refactor test_setting to accomadate new params file

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,5 +1,5 @@
 import pytest
-
+import json
 from src.setting import Setting
 """
 Test for the setting class.
@@ -12,17 +12,13 @@ Test for the setting class.
 """
 # setting_setup_params is the base case for creating the Setting class for testing
 # it covers all "legal" inputs for both the member_id and initial value
+with open("tests/setting_parameters.json", 'r') as setup_params:
+    setting_setup_params = json.load(setup_params)
 
-setting_setup_params = [{"member_id": "test_id", "initial_value": 1}, # "test_id"-member_id | 1-initial_value | "string_int"-test_id
-                        {"member_id": "test_id", "initial_value": 1.0109239},
-                        {"member_id": "test_id", "initial_value": -1.0123},
-                        {"member_id": "test_id", "initial_value": "test_value"},
-                        {"member_id": "test_id", "initial_value": True},
-                        {"member_id": "test_id", "initial_value": False}
-                        ]
-test_id = [f"{param["member_id"]!r}--{type(param["initial_value"]).__name__}" for param in setting_setup_params]
+test_id = [f"{param["member_id"]!r}--{type(param["initial_value"]).__name__}" for param in setting_setup_params["mixed_setup"]]
 
-@pytest.mark.parametrize("params", setting_setup_params, ids=test_id)
+
+@pytest.mark.parametrize("params", setting_setup_params["mixed_setup"], ids=test_id)
 def test_init(setting_example, params):
     """
     Tests that Setting initializes and that both member_id and initial_value only accept valid arguements
@@ -32,7 +28,7 @@ def test_init(setting_example, params):
     assert test_setting.member_id == params["member_id"] and type(test_setting.member_id) == type(params["member_id"]) # Tests that member_id's value and type are correct
     assert test_setting.value == params["initial_value"] and type(test_setting.value) == type(params["initial_value"]) # Tests that initial_value's value and type are correct
 
-@pytest.mark.parametrize("params", setting_setup_params, ids=test_id)
+@pytest.mark.parametrize("params", setting_setup_params["mixed_setup"], ids=test_id)
 def test_str_repr(setting_example, params):
     """
     Tests __str__ and __repr__ for expected results
@@ -41,7 +37,7 @@ def test_str_repr(setting_example, params):
     assert str(test_setting) == str({params["member_id"]: params["initial_value"]})
     assert repr(test_setting) == f"Setting(member_id = {params["member_id"]}, initial_value = {params["initial_value"]})"
 
-@pytest.mark.parametrize("params", setting_setup_params, ids=test_id)
+@pytest.mark.parametrize("params", setting_setup_params["mixed_setup"], ids=test_id)
 def test_call(setting_example, params):
     """
     Tests the call dunder method for changing the value held at member.value
@@ -50,4 +46,3 @@ def test_call(setting_example, params):
     test_setting(params["initial_value"])
 
     assert test_setting.value == params["initial_value"]
-
