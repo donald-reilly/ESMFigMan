6238c1ad48f53d4672bb5c37dc937ed2030fbe3a
Donald Reilly Jr <donald.reilly.jr@outlook.com>
2025-07-29 15:00:01 -0700

    Add: Included member_id in __call__ as a property to be changed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,12 @@ def setting_example():
     A factory fixture for providing controlled instances of the Setting class.
     """
     def _create_setting(**kwargs):
+        """
+        Creates an instance of Setting. If no arguements are provided 
+        then a default setting is created.
+        """
         if kwargs:
-            return Setting(**kwargs)
+            return Setting(**kwargs) # returns a custom setting.
         else:
-            return Setting("default_id", "default_value")
+            return Setting("default_id", "default_value") # returns a default setting.
     return _create_setting
